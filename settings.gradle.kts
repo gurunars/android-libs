47f89014d4394e9d5a4fd5c79ee16499b3206ba6
Anton Berezin <gurunars@gmail.com>
2025-01-01 21:53:20 +0100

    ff

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android-libs" // Your project name
+include(":app_closer")
+include(":state_machine")
+include(":state_machine_runner")
